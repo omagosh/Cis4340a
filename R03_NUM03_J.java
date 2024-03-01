b478e0d894e515a523595a59243a09f34f785e13
oma <omasara17@gmail.com>
2024-02-29 23:25:20 -0500

    FIX NAME

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -1,4 +1,4 @@
-public class Rule03 {
+public class R03_NUM03_J {
     public static long getInteger(DataInputStream is) throws IQException 
     {
         return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
