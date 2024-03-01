1a8f0ee4466290dfe802e613e2a272dcffb31c7f
oma <omasara17@gmail.com>
2024-02-29 22:59:16 -0500

    Rename to correct convention

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -1,11 +1,11 @@
-public class Rule02 {
+public class R02_XP00_J {
     
     public void deleteFile() {
         File someFile = newFile("someFileName.txt");
         // Do something with someFile
         if (!someFile.delete()) {
             // Handle failure to delete the file
-            
+
         }
     }
 }
