4e601ee8c92477381022dde80bf52441b1fb2886
oma <omasara17@gmail.com>
2024-02-29 20:32:56 -0500

    yeee

---
+++ Cis4340.java
@@ -0,0 +1,9 @@
+public class Cis4340 {
+    
+    public static void main(String[] args) {
+
+        //prints "hello world" to terminal window
+
+        System.out.println("Hello, World - from the class of cis4340 - Spring 2024 - UCF Oma Persaud");
+    }
+}
