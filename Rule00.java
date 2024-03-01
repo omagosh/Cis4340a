fcf4290b30cbfa98595f73b1452c6c2218c923c6
oma <omasara17@gmail.com>
2024-02-29 21:50:16 -0500

    Before edits on Rule00

---
+++ Rule00.java
@@ -0,0 +1,14 @@
+public class Rule00 {
+
+    if (login Successful) {
+        logger.severe("User login succeeded for: " + sanitizeUser(username));
+        }
+        else {
+        logger.severe("User login failed for: " + sanitizeUser(username));
+        }
+        
+        public String sanitizeUser(String username) {
+        return Pattern.matches("[A-Za-z0-9]+", username)) ? username: "unauthorized user";
+        }
+}
+        
