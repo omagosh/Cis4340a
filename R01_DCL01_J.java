fe8b65efd6fe2f93c73c54232a9e6760641ed4bb
oma <omasara17@gmail.com>
2024-03-03 20:24:25 -0500

    Noncompliant code

---
+++ R01_DCL01_J.java
@@ -0,0 +1,24 @@
+public class R01_DCL01_J {
+    class Vector {
+        private int val = 1;
+       
+        public boolean isEmpty() {
+          if (val == 1) {   // Compares with 1 instead of 0
+            return true;
+          } else {
+            return false;
+          }
+        }
+        // Other functionality is same as java.util.Vector
+      }
+       
+      // import java.util.Vector; omitted
+      public class VectorUser {
+        public static void main(String[] args) {
+          Vector v = new Vector();
+          if (v.isEmpty()) {
+            System.out.println("Vector is empty");
+          }
+        }
+      }
+}
