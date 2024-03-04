cb3f4fb68954f6d0d940afe4887a0240153de852
oma <omasara17@gmail.com>
2024-03-03 22:33:28 -0500

    noncompliant code

---
+++ R09_LCK04_J.java
@@ -0,0 +1,23 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class R09_LCK04_J {
+    
+private final Map<Integer, String> mapView =
+    Collections.synchronizedMap(new HashMap<Integer, String>());
+private final Set<Integer> setView = mapView.keySet();
+ 
+public Map<Integer, String> getMap() {
+  return mapView;
+}
+ 
+public void doSomething() {
+  synchronized (setView) {  // Incorrectly synchronizes on setView
+    for (Integer k : setView) {
+      // ...
+    }
+  }
+}
+}
