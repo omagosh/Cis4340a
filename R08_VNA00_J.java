3ff5dc0652d5a0147a26ea62a399c05bba8e8594
oma <omasara17@gmail.com>
2024-03-03 22:22:39 -0500

    noncompliant code

---
+++ R08_VNA00_J.java
@@ -0,0 +1,20 @@
+public class R08_VNA00_J {
+    final class ControlledStop implements Runnable {
+        private boolean done = false;
+        
+        @Override public void run() {
+          while (!done) {
+            try {
+              // ...
+              Thread.currentThread().sleep(1000); // Do something
+            } catch(InterruptedException ie) {
+              Thread.currentThread().interrupt(); // Reset interrupted status
+            }
+          }   
+        }
+       
+        public void shutdown() {
+          done = true;
+        }
+      }
+}
